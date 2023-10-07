077833114f9d1e781309de6477a2e977a93c8b6e
https-aya <138739258+https-aya@users.noreply.github.com>
2023-10-07 22:09:11 +0900

    cppへの書き込み
    シーンマネージャーcppへの書き込み

--- Tetris/Tetris/SceneManager.cpp
+++ Tetris/Tetris/SceneManager.cpp
@@ -25,23 +25,120 @@ int read_error;					//読み込みエラー確認
 **********************************/
 
 /**********************************
-*タイトル画面:初期化処理
+*シーン管理機能:初期化処理
 * 引　数:ゲームモード情報
 * 戻り値:なし
 **********************************/
 void SceneManager_Initialize(GAME_MODE mode)
 {
 	read_error = D_NORMALITY;
+	switch (mode)
+	{
+		case E_TITLE:
+			read_error = TitleScene_Initialize();			//タイトル画面の初期化
+			break;
+		case E_GAMEMAIN:
+			read_error = GameMainScene_Initialize();		//ゲームメイン画面の初期化
+			break;
+		case E_RANKING:
+			if (game_mode == E_GAMEMAIN)
+			{
+				Set_RankingMode(RANKING_INPUT_MODE);		//ランキング入力モード
+				Set_RankingScore(Get_Score());				//スコアの取得
+			}
+			else
+			{
+				Set_RankingMode(RANKING_DISP_MODE);			//ランキング描画モードで起動
+			}
+			read_error = RankingScene_Initialize();			//ランキング画面の初期化
+			break;
+		case E_END:
+		default:
+			EndScene_Initialize();							//エンド画面の初期化
+			break;
+	}
+	game_mode = mode;
+	next_mode = game_mode;
 }
 
 /**********************************
-*タイトル画面:更新処理
+*シーン管理機能:更新処理
 * 引　数:なし
 * 戻り値:なし
 **********************************/
+void SceneManager_Update(void)
+{
+	//前フレームとゲームモードが違っていたらシーンを切り替える
+	if (game_mode != next_mode)
+	{
+		SceneManager_Initialize(next_mode);
+	}
+
+	//各画面の更新処理
+	switch (game_mode)
+	{
+		case E_TITLE:
+			TitleScene_Update();
+			break;
+		case E_GAMEMAIN:
+			GameMainScene_Update();
+			break;
+		case E_RANKING:
+			RankingScene_Update();
+			break;
+		case E_END:
+		default:
+			EndScene_Update();
+			break;
+	}
+}
 
 /**********************************
-*タイトル画面:描画処理
+*シーン管理機能:描画処理
 * 引　数:なし
 * 戻り値:なし
 **********************************/
+void SceneManager_Draw(void)
+{
+	//各画面の描画処理
+	switch (game_mode)
+	{
+		case E_TITLE:
+			TitleScene_Draw();
+			break;
+		case E_GAMEMAIN:
+			GameMainScene_Draw();
+			break;
+		case E_RANKING:
+			RankingScene_Draw();
+			break;
+		case E_END:
+		default:
+			EndScene_Draw();
+			break;
+	}
+}
+/**********************************
+*シーン管理機能:シーン現行処理
+* 引　数:変更するゲームモード
+* 戻り値:なし
+**********************************/
+void Change_Scene(GAME_MODE mode)
+{
+	next_mode = mode;
+}
+
+/**********************************
+*シーン管理機能:エラーチェック処理
+* 引　数:なし
+* 戻り値:エラー情報
+**********************************/
+int ErrorCheck(void)
+{
+	if (Get_EndTime() == TRUE)
+	{
+		read_error = D_ERROR;
+	}
+
+	return read_error;
+}
