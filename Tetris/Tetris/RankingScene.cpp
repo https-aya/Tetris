19f1c0aab4197684c355a3fce3112560cc86a32f
https-aya <138739258+https-aya@users.noreply.github.com>
2023-11-14 14:47:07 +0900

    ranking
    rankingの変更

--- Tetris/Tetris/RankingScene.cpp
+++ Tetris/Tetris/RankingScene.cpp
@@ -188,7 +188,8 @@ void file_write(void)
 	{
 		for (i = 0; i < RANKING_MAX; i++)
 		{
-			fprintf(fp, "%2d,%[^,],%10d\n", Ranking_Data[i].rank,
+			//%[^ ]‚ð%s‚É‹A‚é
+			fprintf(fp, "%2d,%s,%10d\n", Ranking_Data[i].rank,
 			Ranking_Data[i].name, Ranking_Data[i].score);
 		}
 
